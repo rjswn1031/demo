4848b7d8addfa1df744f4a14a837e8529813475f
Leekj <rjswn1031@gmail.com>
2023-05-20 16:08:36 +0900

    초기화 커밋

--- demo/src/main/java/com/domain/Code/CodeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.domain.Code;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CodeRepository extends JpaRepository<Code, String> {
-  public List<Code> findAllByCdType(String type);
-}
--- demo/src/main/java/com/domain/parkingInfo/ParkingInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.domain.parkingInfo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParkingInfoRepository extends JpaRepository<ParkingInfo, String> {
-  
-}
